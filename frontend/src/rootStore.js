49502f7a2ffc8fff7dd5fcfcb8dddf4419d0977a
1aS5i-dekYon <https://github.com/1aS5i-dekYon/>
2024-09-21 17:32:47 +0200

    fix rootStore

--- frontend/src/rootStore.js
+++ frontend/src/rootStore.js
@@ -1,9 +1,6 @@
-import { configureStore } from "@reduxjs/toolkit"
-
+import { configureStore } from '@reduxjs/toolkit';
 import { loginFormReducer } from './modules/LoginForm';
 
-export default configureStore({
-    reducer: {
-        loginData: loginFormReducer,
-    }
-})
+export default configureStore(
+  { reducer: { loginData: loginFormReducer } }
+);
