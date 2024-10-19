9817b1cfb6a520a079b9a958c8f01bd42f57c741
1aS5i-dekYon <https://github.com/1aS5i-dekYon/>
2024-10-19 22:12:32 +0200

    add i18next & bg-colors in init.jsx, prepared package.json

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,15 +1,33 @@
 import { Provider } from 'react-redux';
-// import './index.css';
+import i18next from 'i18next';
+import { I18nextProvider, initReactI18next } from 'react-i18next';
 // import './styles.scss';
 // import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
 import App from './App.jsx';
 import store from './rootStore.js';
+import resources from './languages';
 
-const init = async () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+const init = async () => {
+  const i18n = i18next.createInstance();
+
+  await i18n
+    .use(initReactI18next)
+    .init({
+      resources,
+      lng: 'ru',
+      fallbackLng: ['en', 'ru'],
+    });
+  
+  return (
+    <I18nextProvider i18n={i18n}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </I18nextProvider>
+  )
+}
 
 export default init;
