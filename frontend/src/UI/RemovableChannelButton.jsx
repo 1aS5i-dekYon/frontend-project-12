7924dc8af8af1819170a113ad11b3257eae6ff97
1aS5i-dekYon <https://github.com/1aS5i-dekYon/>
2024-10-21 22:43:13 +0200

    connect i18next with ChannelsList UI/

--- frontend/src/UI/RemovableChannelButton.jsx
+++ frontend/src/UI/RemovableChannelButton.jsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next';
 import { Button, ButtonGroup, Dropdown } from 'react-bootstrap';
 
 export default ({
@@ -7,29 +8,33 @@ export default ({
   handleSelect,
   handleRemoveChannel,
   handleRenameChannel,
-}) => (
-  <Dropdown as={ButtonGroup} className="w-100 rounded border border-dark">
-    <Button
-      active={isActive}
-      variant={variant}
-      className="text-start text-truncate me-auto"
-      onClick={handleSelect}
-    >
-      <strong className="h4"># </strong>
-      <span>{buttonName}</span>
-    </Button>
+}) => {
+  const { t } = useTranslation();
 
-    <Dropdown.Toggle
-      active={isActive}
-      split
-      variant={variant}
-      className="border-0 flex-grow-0"
-    >
-      <span className="visually-hidden">Действия над каналом</span>
-    </Dropdown.Toggle>
-    <Dropdown.Menu>
-      <Dropdown.Item onClick={handleRenameChannel}>Переименовать</Dropdown.Item>
-      <Dropdown.Item onClick={handleRemoveChannel}>Удалить</Dropdown.Item>
-    </Dropdown.Menu>
-  </Dropdown>
-);
+  return (
+    <Dropdown as={ButtonGroup} className="w-100 rounded border border-dark">
+      <Button
+        active={isActive}
+        variant={variant}
+        className="text-start text-truncate me-auto"
+        onClick={handleSelect}
+      >
+        <strong className="h4"># </strong>
+        <span>{buttonName}</span>
+      </Button>
+  
+      <Dropdown.Toggle
+        active={isActive}
+        split
+        variant={variant}
+        className="border-0 flex-grow-0"
+      >
+        <span className="visually-hidden">{t('channels.actions')}</span>
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        <Dropdown.Item onClick={handleRenameChannel}>{t('channels.rename')}</Dropdown.Item>
+        <Dropdown.Item onClick={handleRemoveChannel}>{t('channels.remove')}</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
