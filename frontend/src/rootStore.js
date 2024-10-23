29b8e6e1f1f450ead55482e4218b0820c92fa271
1aS5i-dekYon <https://github.com/1aS5i-dekYon/>
2024-10-23 22:40:25 +0200

    connect modalSlice with rootStore & fix ChatHeader

--- frontend/src/rootStore.js
+++ frontend/src/rootStore.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import { loginFormReducer } from './modules/LoginForm';
 import { channelsReducer } from './modules/ChannelsList';
 import { messagesReducer } from './modules/ChatArea';
+import { modalReducer } from './modules/Modals';
 
 export default configureStore({
   reducer: {
     loginData: loginFormReducer,
     messages: messagesReducer,
     channels: channelsReducer,
+    modal: modalReducer,
   },
 });
